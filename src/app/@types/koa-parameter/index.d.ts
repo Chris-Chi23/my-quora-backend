c6dc520591698b6e920b81d34efbf4dca1cab52b
Chris-Chi23 <xianming.chi@gmail.com>
2020-09-05 23:00:50 -0400

    'ss'

--- src/app/@types/koa-parameter/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'koa-parameter' {
-    import Application from "koa";
-    export default function(app: Application, translate?: string) : (ctx: Application.Context, next: Application.Next) => void;
-}
--- src/app/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-import f = require("./home");
-import www from './home';
-
-const router = new Router({prefix: '/users'});
-router.get('/', ctx => {
-});
-router.post('/', ctx => {
-});
